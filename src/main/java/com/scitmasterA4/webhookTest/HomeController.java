d6057426b484a1b712e2735525befde159431f68
Junwoong Cha <chawnsnd@gmail.com>
2020-02-22 15:32:56 +0900

    핑퐁 테스트

--- src/main/java/com/scitmasterA4/webhookTest/HomeController.java
+++ src/main/java/com/scitmasterA4/webhookTest/HomeController.java
@@ -2,6 +2,7 @@ package com.scitmasterA4.webhookTest;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -10,6 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Handles requests for the application home page.
@@ -37,7 +42,8 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/papago", method = RequestMethod.POST)
-	public void papago(String token, String teamName, String roomName, String writerName, String text, String keyword,
+	@ResponseBody
+	public String papago(String token, String teamName, String roomName, String writerName, String text, String keyword,
 			String createdAt) {
 		logger.debug(token);
 		logger.debug(teamName);
@@ -46,5 +52,28 @@ public class HomeController {
 		logger.debug(text);
 		logger.debug(keyword);
 		logger.debug(createdAt);
+		
+//		{
+//			"body" : "[[PizzaHouse]](http://url_to_text) You have a new Pizza order.",
+//			"connectColor" : "#FAC11B",
+//			"connectInfo" : [{
+//			"title" : "Topping",
+//			"description" : "Pepperoni"
+//			},
+//			{
+//			"title": "Location",
+//			"description": "Empire State Building, 5th Ave, New York",
+//			}]
+//			}
+		String json = "";
+		ObjectMapper mapper = new ObjectMapper();
+		HashMap<String, String> data = new HashMap<String, String>();
+		data.put("body", "[핑퐁테스트]"+text);
+		try {
+			json = mapper.writeValueAsString(data);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return json;
 	}
 }
